af9a7ce44b3e4b80cc2e3191bfe2add738f9884e
Samuel Madigage <samlambo929@gmail.com>
2019-07-15 14:05:32 +0200

    Updates

---
+++ precache-manifest.8e9b1fe784096830603cdd93387bb6ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee08816465af723ad39d7fbaeb08b9c",
+    "url": "/Crypto_Dash/index.html"
+  },
+  {
+    "revision": "7a75f26c3fb4f8182ae1",
+    "url": "/Crypto_Dash/static/css/main.641b4306.chunk.css"
+  },
+  {
+    "revision": "055bd1a0af9cfa701287",
+    "url": "/Crypto_Dash/static/js/2.9df2bf1d.chunk.js"
+  },
+  {
+    "revision": "7a75f26c3fb4f8182ae1",
+    "url": "/Crypto_Dash/static/js/main.f95cf0ea.chunk.js"
+  },
+  {
+    "revision": "8846f1ba4e12a99a5013",
+    "url": "/Crypto_Dash/static/js/runtime~main.77310950.js"
+  }
+]);
